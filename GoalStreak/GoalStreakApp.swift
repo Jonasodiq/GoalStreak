e744c9749d7cd94a3b52c42b49b1e8ceb42bca41
Jonasodiq <jonasniyazson@hotmail.com>
2025-04-29 14:09:16 +0200

    Initial Commit

---
+++ GoalStreak/GoalStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalStreakApp.swift
+//  GoalStreak
+//
+//  Created by Jonas Niyazson on 2025-04-29.
+//
+
+import SwiftUI
+
+@main
+struct GoalStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
