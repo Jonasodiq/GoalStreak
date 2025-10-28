763ca6ac3702f6eef43cc35e626d612d9d2db7c6
Jonasodiq <jonasniyazson@hotmail.com>
2025-10-28 23:07:04 +0100

    Added GoalLog Double

--- GoalStreak/Models/GoalLog.swift
+++ GoalStreak/Models/GoalLog.swift
@@ -12,4 +12,5 @@ struct GoalLog: Identifiable, Codable {
     @DocumentID var id: String?
     var goalId: String
     var timestamp: Date
+    var value: Double // hur mycket användaren gjorde (t.ex. 5 min, 2 km, osv.)
 }
